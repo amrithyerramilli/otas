9cf895518886b3916b5973c1408bb316d5065b74
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-04-28 04:48:16 +0000

    Test project updating to google code using SVN --refs #1

---
+++ trunk/hello world/hello world/hello world.cpp
@@ -0,0 +1,12 @@
+// hello world.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello world";
+	return 0;
+}
+
