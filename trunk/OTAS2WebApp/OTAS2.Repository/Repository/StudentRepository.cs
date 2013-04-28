4dc8aacef1898ef0c0ea2c45cd76c33b7762438c
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-04-28 14:42:43 +0000

    Added main project --refs #5

---
+++ trunk/OTAS2WebApp/OTAS2.Repository/Repository/StudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTAS2.Repository.Interfaces;
+using OTAS2.Domain.Entities;
+
+namespace OTAS2.Repository.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        IContext context;
+        public StudentRepository()
+            : this(new Context())
+        {
+        }
+        public StudentRepository(IContext context)
+        {
+            this.context = context;
+        }
+        public IList<Student> GetAllStudents()
+        {
+            return context.Students.ToList();
+        }
+    }
+}
---
+++ trunk/OTAS2WebApp/OTAS2WebApp/Areas/Student/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OTAS2.Repository.Repository;
+
+
+namespace OTAS2WebApp.Areas.Student.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Student/Login/
+
+        public ActionResult Login()
+        {
+            return View("Login/Login");
+        }
+
+        public ActionResult Details()
+        {
+            return View("Details/Details");
+                        
+        }
+        public ActionResult PasswordValidation(FormCollection form)
+        {
+            string username = form["txtUsername"];
+            var password = form["txtPassword"];
+            StudentRepository s = new StudentRepository();
+            var studs = (from i in s.GetAllStudents()
+                        where i.USN == username
+                        select i).ToList();
+
+            return this.RedirectToAction("Details");
+        }
+
+    }
+}
