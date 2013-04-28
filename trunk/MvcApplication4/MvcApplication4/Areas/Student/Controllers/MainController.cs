ef03bebdd3c98819b91f0c1ed2895119672da6b8
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-04-28 10:22:03 +0000

    added login feature --refs #4

---
+++ trunk/MvcApplication4/MvcApplication4/Areas/Student/Controllers/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Areas.Student.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Student/Login/
+
+        public ActionResult Login()
+        {
+            return View("Login/Login");
+        }
+
+        public ActionResult Details()
+        {
+            return View("Details/Details");
+                        
+        }
+        public ActionResult PasswordValidation(FormCollection form)
+        {
+            var a = form["txtUsername"];
+            return this.RedirectToAction("Details");
+        }
+
+    }
+}
