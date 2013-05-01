3c1856e56b835dbeb3b544c8fdc3c2fe467be35f
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-01 10:54:06 +0000

    Added mailing facility --refs #7

---
+++ trunk/OTAS2WebApp/OTAS2.MailApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+using OTAS2.Domain.Entities;
+using OTAS2.Repository.Repository;
+
+namespace OTAS2.MailApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentRepository students = new StudentRepository();
+            ValidSRepository valids = new ValidSRepository();
+            var student = (from i in students.GetAllStudents()
+                           select i).ToList();
+            var valid = (from i in valids.GetAllValidS()
+                         select i).ToList();
+            var studpass = (from i in student
+                            join j in valid on i.USN equals j.USN
+                            where i.email != null
+                            select new StudentPassword { USN = j.USN, Password = j.PassGen, Email = i.email.ToLower() }).ToList();
+            Attachment picture = new Attachment("Appraisal.gif");
+            //i = 1350;
+            for (int i = 1351; i < 1800; i++)
+            {
+                MailAddress to;
+                if (studpass[i].Email != "")
+                {
+                    Console.WriteLine("Mail To");
+                    to = new MailAddress(studpass[i].Email);
+                }
+                else
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Mail From");
+                MailAddress from = new MailAddress("otas@rnsit.ac.in");
+
+                MailMessage mail = new MailMessage(from, to);
+
+                Console.WriteLine("Subject");
+                mail.Subject = "RNSIT Appraisal";
+
+                Console.WriteLine("Your Message");
+                mail.Body = "hola amigo,\n Your Password for teachers appraisal is : " + studpass[i].Password + " , please note it down and arrive on May 2nd/May 3rd.\n Pass on this link to who ever hasn't provided us with their details the first time around https://docs.google.com/forms/d/1LSIUQKa9bgGx7E3TlkUhktkXP80rAPtcnRdu6ltIIrY/viewform \n Best,\n Aikya Dev Team";
+                mail.Attachments.Add(picture);
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+
+                smtp.Credentials = new NetworkCredential(
+                    "anirudh.1rn09cs012@gmail.com", "9742245270");
+                smtp.EnableSsl = true;
+                Console.WriteLine("Sending email...");
+                smtp.Send(mail);
+                Console.WriteLine("" + i);
+            }
+        }
+    }
+}
