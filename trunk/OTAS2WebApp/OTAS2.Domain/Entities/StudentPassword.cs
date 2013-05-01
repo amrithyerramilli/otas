cb02496631500bc6380873344f00b9e2704a45ad
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-01 10:58:13 +0000

    --refs #7

---
+++ trunk/OTAS2WebApp/OTAS2.Domain/Entities/StudentPassword.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OTAS2.Domain.Entities
+{
+    public class StudentPassword
+    {
+        public string USN { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
