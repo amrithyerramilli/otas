95f69f57ed57f10bb82f15fba1e2f62c83c8d79f
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-22 09:51:57 +0000

    added repository

---
+++ trunk/OTAS2WebApp/OTAS2.Repository/Repository/SubjectRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTAS2.Domain.Entities;
+using OTAS2.Repository.Interfaces;
+
+namespace OTAS2.Repository.Repository
+{
+    public class SubjectRepository : ISubjectRepository
+    {
+        IContext context;
+        public SubjectRepository()
+            : this(new Context())
+        {
+        }
+        public SubjectRepository(IContext context)
+        {
+            this.context = context;
+        }
+        public IList<Subject> GetAllSubjects()
+        {
+            return context.Subjects.ToList();
+        }
+    }
+}
---
+++ trunk/OTAS2WebApp/OTAS2.Repository/Repository/TeacherInfoRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTAS2.Domain.Entities;
+using OTAS2.Repository.Interfaces;
+
+namespace OTAS2.Repository.Repository
+{
+    public class TeacherInfoRepository : ITeacherInfoRepository
+    {
+        IContext context;
+        public TeacherInfoRepository()
+            : this(new Context())
+        {
+        }
+        public TeacherInfoRepository(IContext context)
+        {
+            this.context = context;
+        }
+        public IList<TeacherInfo> GetAllTeacherInfo()
+        {
+            return context.TeacherInfo.ToList();
+        }
+    }
+}
