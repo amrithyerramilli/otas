c2cd07da20ba0576d62a9755d136b78ede3d5e60
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-22 09:51:57 +0000

    added repository

---
+++ OTAS2WebApp/OTAS2.Repository/Repository/SubCompRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTAS2.Domain.Entities;
+using OTAS2.Repository.Interfaces;
+
+namespace OTAS2.Repository.Repository
+{
+    public class SubCompRepository : ISubCombRepository
+    {
+        IContext context;
+        public SubCompRepository()
+            : this(new Context())
+        {
+        }
+        public SubCompRepository(IContext context)
+        {
+            this.context = context;
+        }
+        public IList<SubComb> GetAllSubComb()
+        {
+            return context.SubComb.ToList();
+        }
+    }
+}
