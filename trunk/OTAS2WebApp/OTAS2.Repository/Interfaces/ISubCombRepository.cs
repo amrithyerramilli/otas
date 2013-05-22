ef71ce61cee3ddff9158b15f744e662ce9c1e60a
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-22 09:51:20 +0000

    added repos

---
+++ trunk/OTAS2WebApp/OTAS2.Repository/Interfaces/ISubCombRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTAS2.Domain.Entities;
+
+namespace OTAS2.Repository.Interfaces
+{
+    public interface ISubCombRepository
+    {
+        IList<SubComb> GetAllSubComb();
+    }
+}
