382975cd9179897de641b6ac3a67414c634d1d79
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-23 07:32:41 +0000

    Added new files

---
+++ OTAS2WebApp/OTAS2.Domain/Entities/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace OTAS2.Domain.Entities
+{
+    [Table("Subject")]
+    public class Subject
+    {
+        [Key]
+        public string SubCode { get; set; }
+        public string SubName { get; set; }
+        public int Sem { get; set; }
+        public string DeptId { get; set; }
+        public int? elective { get; set; }
+    }
+}
---
+++ OTAS2WebApp/OTAS2.Domain/Entities/TeacherInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace OTAS2.Domain.Entities
+{
+    [Table("TeacherInfo")]
+    public class TeacherInfo
+    {
+        [Key]
+        public string TeacherId { get; set; }
+        public string TeacherName { get; set; }
+        public string DeptId { get; set; }
+        public string Designation { get; set; }
+    }
+}
