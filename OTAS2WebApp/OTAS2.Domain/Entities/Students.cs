97d9a13fc24a3dc4d3cd7810c67448a21ed3351b
amrith.yerramilli@gmail.com <amrith.yerramilli@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-23 10:43:36 +0000

    removed bugs in summary

--- OTAS2WebApp/OTAS2.Domain/Entities/Students.cs
+++ OTAS2WebApp/OTAS2.Domain/Entities/Students.cs
@@ -6,15 +6,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace OTAS2.Domain.Entities
 {
-   public class Students
-   {
+    public class Students
+    {
         [Key]
         public String USN { get; set; }
         public String Name { get; set; }
         public String Address { get; set; }
         public String email { get; set; }
-        public String Sec { get; set; }
+        public String Sec { get; set; }      
+        [NotMapped]
+        public string Elective1Name { get; set; }
         public String Elective1 { get; set; }
+        [NotMapped]
+        public string Elective2Name { get; set; }
         public String Elective2 { get; set; }
         public String DeptID { get; set; }
         public int? sem { get; set; }
