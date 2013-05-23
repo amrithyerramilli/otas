0dc8ba13ce358fef41f17f74d04ab42a6acbf83e
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-05-23 07:32:41 +0000

    Added new files

---
+++ trunk/OTAS2WebApp/OTAS2.Domain/Entities/TeacherSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OTAS2.Domain.Entities
+{
+    public class TeacherSummary
+    {
+        public string StudentUSN { get; set; }
+        public string ComboId { get; set; }
+        public string TeacherName { get; set; }
+        public string SubName { get; set; }
+        public string SubCode { get; set; }
+    }
+}
