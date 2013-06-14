2915cbe5bbb96ecf0be5b97719bdee4e0e601512
anirudh.kkw@gmail.com <anirudh.kkw@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-06-14 06:25:15 +0000

    Added user edit write back to DB

--- OTAS2WebApp/OTAS2.Repository/Repository/StudentRepository.cs
+++ OTAS2WebApp/OTAS2.Repository/Repository/StudentRepository.cs
@@ -4,23 +4,45 @@ using System.Linq;
 using System.Text;
 using OTAS2.Repository.Interfaces;
 using OTAS2.Domain.Entities;
+using System.Data;
 
 namespace OTAS2.Repository.Repository
 {
     public class StudentRepository : IStudentRepository
     {
-        IContext context;
+        Context context;
         public StudentRepository()
             : this(new Context())
         {
         }
-        public StudentRepository(IContext context)
+        public StudentRepository(Context context)
         {
             this.context = context;
         }
         public IList<Students> GetAllStudents()
         {
             return context.Students.ToList();
+        }
+        public void UpdateStudent(Students record)
+        {
+            context.Entry(record).State = EntityState.Modified;
+            Save();
+        }
+        public void Save()
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
     }
 }
