d7801d099aba197919aaad19677eccde99a36742
amrith.yerramilli@gmail.com <amrith.yerramilli@gmail.com@ce352332-3d41-7751-9a9c-5ba577f1960a>
2013-06-14 06:22:52 +0000

    Error and feedback

---
+++ trunk/OTAS2WebApp/OTAS2.Domain/Entities/Feedback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace OTAS2.Domain.Entities
+{
+    [Table("Feedback")]
+    public class Feedback
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Value { get; set; }
+        public string Suggestion { get; set; }
+        
+    }
+}
---
+++ trunk/OTAS2WebApp/OTAS2.Repository/Repository/FeedbackRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OTAS2.Domain.Entities;
+using OTAS2.Repository.Interfaces;
+
+namespace OTAS2.Repository.Repository
+{
+    public class FeedbackRepository
+    {
+        Context context;
+        public FeedbackRepository()
+            : this(new Context())
+        {
+        }
+        public FeedbackRepository(Context context)
+        {
+            this.context = context;
+        }
+        public void InsertRecord(Feedback record)
+        {
+            context.Feedback.Add(record);
+            Save();
+        }
+        public void Save()
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
